ac6f841e54ce6281030d0ee6bf1f2d8f0fe17f93
Luke Chen Shui <chenshuiluke@gmail.com>
2016-12-21 20:45:45 -0500

    Added route for docs in routes.php

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,10 @@ Router::get('/privacy_policy.html', function(){
     return new View("privacy_policy");
 });
 
+Router::get('/docs', function(){
+    return new View("docs/index");
+});
+
 Router::get('/users/get', 'UserController::find');
 Router::get('/users/get/all', 'UserController::findAll');
 Router::post('/users/add', 'UserController::create');
